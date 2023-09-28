28f7b6137cbbb8d1a08bfe07c7e98adde269ba4a
rbanore <47154037+rishikeshbanore@users.noreply.github.com>
2023-09-28 23:24:55 +0200

    Added Delete Method and Fluent Assertion for Status code.

---
+++ API Testing with Playwright/Tests/DELETEMethod.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using FluentAssertions;
+
+
+namespace API_Testing_with_Playwright.Tests
+{
+        public class DELETEMethod : IClassFixture<PlaywrightDriver>
+        {
+            private readonly PlaywrightDriver _playwrightDriver;
+            public DELETEMethod(PlaywrightDriver playwrightDriver)
+            {
+                _playwrightDriver = playwrightDriver;
+            }
+
+             [Fact]
+             public async Task GetCartOfUser_5()
+             {
+                   var DeleteProducttResponse = await _playwrightDriver?.APIRequestContext?.DeleteAsync("/products/1");
+                   var DeleteProductResponseData = await DeleteProducttResponse.JsonAsync();
+
+                    DeleteProducttResponse.Status.Should().Be(200);
+              }
+        }
+}
